ecaa26ba3299a1fc66e2ac3dffbf7b75fc68c406
niculas1989 <nicolas.maranzano@libero.it>
2022-03-22 15:25:40 +0100

    added list of comics in INDEX - all works

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.main')
 
-@section('title', 'Comics')
+@section('title', 'All Comics')
 
 @section('content')
 <div class="container">
@@ -8,7 +8,14 @@
     <div class="row">
         @foreach($comics as $comic)
         <div class="col-2">
-            <strong>{{ $comic['title'] }}</strong>
+            <div class="card" style="width: 18rem;">
+                <img src="{{ $comic['thumb'] }}" class="card-img-top img-fluid" alt="{{ $comic['title'] }}">
+                <div class="card-body">
+                    <h5 class="card-title">{{ $comic['title'] }}</h5>
+                    <p class="card-text">{{ $comic['description'] }}</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
         </div>
         @endforeach
     </div>
