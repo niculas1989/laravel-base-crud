05107ab23f3a2fec5e51369ea808e1e23bc920d4
niculas1989 <nicolas.maranzano@libero.it>
2022-03-22 15:29:37 +0100

    added more info for INDEX

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -8,11 +8,12 @@
     <div class="row">
         @foreach($comics as $comic)
         <div class="col-2">
-            <div class="card" style="width: 18rem;">
+            <div class="card">
                 <img src="{{ $comic['thumb'] }}" class="card-img-top img-fluid" alt="{{ $comic['title'] }}">
                 <div class="card-body">
                     <h5 class="card-title">{{ $comic['title'] }}</h5>
-                    <p class="card-text">{{ $comic['description'] }}</p>
+                    <p class="card-text">${{ $comic['series'] }}</p>
+                    <p class="card-text">${{ $comic['price'] }}</p>
                     <a href="#" class="btn btn-primary">Go somewhere</a>
                 </div>
             </div>
